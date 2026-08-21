1e345a3d0fdb7b92052bc1d06da8a919ca7e3344
c-lech <126396070+c-lech@users.noreply.github.com>
2026-08-21 13:19:54 -0300

    update

--- install.sh
+++ install.sh
@@ -266,8 +266,6 @@ install_ssh() {
   step "Copying id_ed25519.pub"
   cp "$src/id_ed25519.pub" "$HOME/.ssh/"
   chmod 644 "$HOME/.ssh/id_ed25519.pub"
-  step "Creating known_hosts symlink"
-  ln -sf /mnt/c/data/projects/infra/wsl_ssh_key/known_hosts "$HOME/.ssh/known_hosts"
   step "Done"
 }
 
