6e4ee80b382aa2f7d5b4ac6968af80b519986eba
c-lech <126396070+c-lech@users.noreply.github.com>
2026-09-11 00:01:17 -0300

    update

--- install.sh
+++ install.sh
@@ -1306,7 +1306,7 @@ main() {
   run_step "tools:fastfetch" "Installing Fastfetch" --quiet install_fastfetch
   run_step "tools:agent" "Installing opencode" --quiet install_opencode
   run_step "tools:tmuxai" "Installing tmuxai" --quiet install_tmuxai
-  run_step "tools:ollama" "Installing Ollama" --quiet install_ollama
+  #run_step "tools:ollama" "Installing Ollama" --quiet install_ollama
   run_step "tools:vagrant" "Installing vagrant" --quiet install_vagrant
   run_step "tools:cliamp" "Installing cliamp" --quiet install_cliamp
   run_step "tools:golazo" "Installing golazo" --quiet install_golazo
